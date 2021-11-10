9e3dca6fdffbd642de713b9085adcb2700908bcf
Mqondisi Ndlovu <mndlovu@axissol.com>
2021-11-10 18:16:36 +0200

    add resources and mvc for Requests & Transactions

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -152,7 +152,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-master@245e5cfe8808c203855cccd4f77185180c15a530',
+  'laravel/laravel' => 'dev-master@7efd621a28d73c2ed4af964692b47618468bf91c',
 );
 
     private function __construct()
