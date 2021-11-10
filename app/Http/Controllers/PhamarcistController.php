8df82b4ca746340c9f0c7909767a0a0886ea1e43
Mqondisi Ndlovu <mndlovu@axissol.com>
2021-11-10 18:05:30 +0200

    add mvc for Doctor, Patient & Pharmacist

---
+++ app/Http/Controllers/PhamarcistController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Phamarcist;
+use Illuminate\Http\Request;
+
+class PhamarcistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Phamarcist  $phamarcist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Phamarcist $phamarcist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Phamarcist  $phamarcist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Phamarcist $phamarcist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Phamarcist  $phamarcist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Phamarcist $phamarcist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Phamarcist  $phamarcist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Phamarcist $phamarcist)
+    {
+        //
+    }
+}
